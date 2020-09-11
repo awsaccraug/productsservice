48850416c1a1115b06abc479ab5a0cfb27b604c5
Emmanuel Koomson <emmanuelkoomson.forever@gmail.com>
2020-09-11 18:42:38 +0000

    first commit

---
+++ remove.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# IMPORTANT: Bucket names must be unique for all AWS users.
+BUCKET="pet-store-api-deployment-workspace-UNIQUE_NAME"
+
+# Delete CloudFormation Stack
+aws cloudformation delete-stack \
+    --stack-name pet-store-stack
+
+# Delete non-empty bucket
+aws s3 rb s3://$BUCKET --force
+© 2020 GitHub, Inc.
